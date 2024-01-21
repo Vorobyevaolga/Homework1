6404af0b1bd23d2e92fa424958ac9e56edf62d37
MI <Bocha555olga@yandex.ru>
2024-01-21 19:39:44 +0400

    HW_var_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, удивительный мир!");
+        var dog=8.0; //собака
+        var cat=3.6; //кошка
+        var paper=763789; //бумага
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
