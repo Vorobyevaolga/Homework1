6556fd437a8553516809ec38da59a9de9d75b983
MI <Bocha555olga@yandex.ru>
2024-02-16 01:05:01 +0400

    HW_char

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] reverseFullName = {'r', 'o', 'g', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
