3c7ddc3e6273d04c96fbb576c611b3e717b21379
MI <Bocha555olga@yandex.ru>
2024-03-03 13:28:30 +0400

    HW_object_1_privat

---
+++ src/Author.java
@@ -0,0 +1,24 @@
+public class Author {
+
+    private String name;
+    private String lastName;
+    private int age;
+
+    public Author(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastNameName() {
+        return this.lastName;
+    }
+
+    public int getage() {
+        return this.age;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    private String nameBook;
+    private Author nameAuthor;
+    private int yearBook;
+    public Book(String nameBook, Author nameAuthor, int yearBook){
+        this.nameBook=nameBook;
+        this.nameAuthor=nameAuthor;
+        this.yearBook=yearBook;
+    }
+    public String getNameBook() {
+        return this.nameBook;
+    }
+   public int getyearBook() {
+        return this.yearBook;
+    }
+    public void setYearBooke(int yearBook) {
+        this.yearBook = yearBook;
+    }
+}
