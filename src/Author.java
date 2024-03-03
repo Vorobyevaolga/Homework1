f47fc621b764463613bb213d46b7be166bbe4de2
MI <Bocha555olga@yandex.ru>
2024-03-03 13:28:30 +0400

    HW_object_1

---
+++ src/Author.java
@@ -0,0 +1,24 @@
+public class Author {
+
+    String name;
+    String lastName;
+    int age;
+
+    public Author(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastNameName() {
+        return this.lastName;
+    }
+
+    public int getage() {
+        return this.age;
+    }
+}
